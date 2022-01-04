e0b7f6867fc1bfd8674abbf15775f753c739f3a5
Raul Anghel <raul.anghel@gmail.com>
2022-01-04 13:21:33 +0200

    1st

--- Python/test.py
+++ Python/test.py
@@ -1,1 +1,15 @@
 print("test")
+
+for i in range(5):
+    print(i * 3)
+
+x = ["a", "b", "c"]
+y = [1, 2, 3]
+z = x + y
+
+print(len(z), z)
+
+
+class Mouse:
+    def __init__(self) -> None:
+        self.feet = 4
