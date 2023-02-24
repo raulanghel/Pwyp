62a44bd0836e5e56a177810fbd4ada06de1abce1
Raul Anghel <raul.anghel@gmail.com>
2023-02-24 13:00:17 +0200

    some more stuff

---
+++ Golang/MasteringGo/01_AQuickIntroductionToGo/Which/which.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	arguments := os.Args
+	if len(arguments) == 1 {
+		fmt.Println("Please provide an argument")
+		return
+	}
+	file := arguments[1]
+	path := os.Getenv("PATH")
+	pathSplit := filepath.SplitList(path)
+	for _, directory := range pathSplit {
+		fullPath := filepath.Join(directory, file)
+		// does it exist?
+		fileInfo, err := os.Stat(fullPath)
+		if err == nil {
+			mode := fileInfo.Mode()
+			// is it a regular file?
+			if mode.IsRegular() {
+				// is it executable?
+				if mode&0111 != 0 {
+					fmt.Println(fullPath)
+				}
+			}
+		}
+	}
+}
